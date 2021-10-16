ddbf0c7dc6b64e3a5f86985ebceb8d078bb44812
Venkhat Balaji <59576722+VenkhatBalaji95@users.noreply.github.com>
2021-10-16 22:43:23 +0530

    Update hackerRank2.py

--- hackerRank2.py
+++ hackerRank2.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
             c = s.count(i)
             b[i] = c
     a = sorted(b.items(), key=lambda x: x[1], reverse=True)
-    print (a)
     for i in a[:3]:
         print (i[0], i[1])
       
