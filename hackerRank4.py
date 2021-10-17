0a8783523809c3ebbb7aba2d525ef59681e02ae0
Venkhat Balaji <venkatbalaji372@gmail.com>
2021-10-17 12:56:48 +0530

    Create hackerRank4.py

---
+++ hackerRank4.py
@@ -0,0 +1,15 @@
+from itertools import *
+
+a,b = map(int,input().split())
+z = []
+
+for i in range(a):
+    c = list(map(int,input().split()))[:7]
+    z.append(c)
+print (z)
+MAX = -1
+for i in product(*z):
+    MAX = max(sum(map(lambda x: x*x, i))%b,MAX)
+print (MAX)
+
+#Link: https://www.hackerrank.com/challenges/maximize-it/problem
