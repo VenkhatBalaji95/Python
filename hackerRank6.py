ba3ed7ea0f262cff7e1fa1d4f7dd63c9c003c501
Venkhat Balaji <venkatbalaji372@gmail.com>
2021-10-18 16:47:23 +0530

    Create hackerRank6.py

---
+++ hackerRank6.py
@@ -0,0 +1,10 @@
+n = int(input()) or 3
+z = dict()
+for i in range(n):
+     n, *m = input().split()
+     print (m)
+     s = list(map(float,m))
+     z[n] = s
+u = input() or "Harsh"
+final = sum(z[u])/len(z[u])
+print ("{:.2f}".format(final))
